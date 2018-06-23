849f9486d8ee05ada7f049be91b0a5a2b71acaee
Kyle E. Mitchell <kyle@kemitchell.com>
2018-06-23 14:45:02 -0700

    Pull buy API request out to api package

---
+++ api/buy.go
@@ -0,0 +1,46 @@
+package api
+
+import "bytes"
+import "encoding/json"
+import "errors"
+import "github.com/licensezero/cli/data"
+import "io/ioutil"
+import "net/http"
+
+type BuyRequest struct {
+	Action       string   `json:"action"`
+	Projects     []string `json:"projects"`
+	Name         string   `json:"licensee"`
+	Jurisdiction string   `json:"jurisdiction"`
+	EMail        string   `json:"email"`
+	Person       string   `json:"person"`
+}
+
+type BuyResponse struct {
+	Location string `json:"location"`
+}
+
+func Buy(identity *data.Identity, projectIDs []string) (string, error) {
+	bodyData := BuyRequest{
+		Action:       "order",
+		Projects:     projectIDs,
+		Name:         identity.Name,
+		Jurisdiction: identity.Jurisdiction,
+		EMail:        identity.EMail,
+		Person:       "I am a person, not a legal entity.",
+	}
+	body, err := json.Marshal(bodyData)
+	if err != nil {
+		return "", errors.New("could not construct quote request")
+	}
+	response, err := http.Post("https://licensezero.com/api/v0", "application/json", bytes.NewBuffer(body))
+	defer response.Body.Close()
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", errors.New("invalid server response")
+	}
+	var parsed BuyResponse
+	json.Unmarshal(responseBody, &parsed)
+	location := parsed.Location
+	return "https://licensezero.com" + location, nil
+}
--- subcommands/buy.go
+++ subcommands/buy.go
@@ -1,28 +1,12 @@
 package subcommands
 
-import "bytes"
-import "encoding/json"
 import "flag"
 import "fmt"
 import "github.com/licensezero/cli/inventory"
+import "github.com/licensezero/cli/api"
 import "github.com/licensezero/cli/data"
 import "github.com/skratchdot/open-golang/open"
-import "io/ioutil"
-import "net/http"
 import "os"
-
-type BuyRequest struct {
-	Action       string   `json:"action"`
-	Projects     []string `json:"projects"`
-	Name         string   `json:"licensee"`
-	Jurisdiction string   `json:"jurisdiction"`
-	EMail        string   `json:"email"`
-	Person       string   `json:"person"`
-}
-
-type BuyResponse struct {
-	Location string `json:"location"`
-}
 
 var Buy = Subcommand{
 	Description: "Buy missing private licenses.",
@@ -56,33 +40,14 @@ var Buy = Subcommand{
 			for _, project := range unlicensed {
 				projectIDs = append(projectIDs, project.Envelope.Manifest.ProjectID)
 			}
-			bodyData := BuyRequest{
-				Action:       "order",
-				Projects:     projectIDs,
-				Name:         identity.Name,
-				Jurisdiction: identity.Jurisdiction,
-				EMail:        identity.EMail,
-				Person:       "I am a person, not a legal entity.",
-			}
-			body, err := json.Marshal(bodyData)
+			location, err := api.Buy(identity, projectIDs)
 			if err != nil {
-				os.Stderr.WriteString("Could not construct quote request.")
+				fmt.Println(err.Error())
 				os.Exit(1)
 			}
-			response, err := http.Post("https://licensezero.com/api/v0", "application/json", bytes.NewBuffer(body))
-			defer response.Body.Close()
-			responseBody, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				os.Stderr.WriteString("Invalid server response.\n")
-				os.Exit(1)
-			}
-			var parsed BuyResponse
-			json.Unmarshal(responseBody, &parsed)
-			location := parsed.Location
-			url := "https://licensezero.com" + location
-			os.Stdout.WriteString(url + "\n")
+			os.Stdout.WriteString(location + "\n")
 			if !*doNotOpen {
-				open.Run(url)
+				open.Run(location)
 			}
 			os.Exit(0)
 		}
