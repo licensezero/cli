67e1236851b1d9313f47f1e7e6eb8d4916149587
Kyle E. Mitchell <kyle@kemitchell.com>
2018-06-22 19:40:07 -0700

    Split inventory package into multiple files

--- inventory/inventory.go
+++ inventory/inventory.go
@@ -1,13 +1,8 @@
 package inventory
 
-import "encoding/json"
 import "github.com/licensezero/cli/data"
-import "io/ioutil"
-import "log"
 import "os"
 import "path"
-import "strings"
-import "github.com/yookoala/realpath"
 
 type Project struct {
 	Type     string
@@ -16,18 +11,6 @@ type Project struct {
 	Name     string
 	Version  string
 	Manifest ProjectManifest
-}
-
-type PackageJSONFile struct {
-	Name      string                   `json:"name"`
-	Version   string                   `json:"version"`
-	Envelopes []ProjectManifestEnvlope `json:"licensezero"`
-}
-
-type ProjectManifestEnvlope struct {
-	LicensorSignature string          `json:"agentSignature"`
-	AgentSignature    string          `json:"licensorSignature"`
-	Manifest          ProjectManifest `json:"license"`
 }
 
 type ProjectManifest struct {
@@ -131,117 +114,6 @@ func ValidMetadata(result *Project) bool {
 
 func ReadProjects(cwd string) ([]Project, error) {
 	return ReadNPMProjects(cwd)
-}
-
-func ReadNPMProjects(packagePath string) ([]Project, error) {
-	var returned []Project
-	node_modules := path.Join(packagePath, "node_modules")
-	entries, err := readAndStatDir(node_modules)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return []Project{}, nil
-		} else {
-			return nil, err
-		}
-	}
-	processProject := func(directory string, scope *string) error {
-		package_json := path.Join(directory, "package.json")
-		data, err := ioutil.ReadFile(package_json)
-		if err != nil {
-			return err
-		}
-		var parsed PackageJSONFile
-		json.Unmarshal(data, &parsed)
-		anyNewProjects := false
-		for _, envelope := range parsed.Envelopes {
-			if alreadyHaveProject(returned, envelope.Manifest.ProjectID) {
-				continue
-			}
-			anyNewProjects = true
-			project := Project{
-				Type:     "npm",
-				Path:     directory,
-				Name:     parsed.Name,
-				Version:  parsed.Version,
-				Manifest: envelope.Manifest,
-			}
-			realDirectory, err := realpath.Realpath(directory)
-			if err != nil {
-				project.Path = realDirectory
-			} else {
-				project.Path = directory
-			}
-			if scope != nil {
-				project.Scope = *scope
-			}
-			returned = append(returned, project)
-		}
-		if anyNewProjects {
-			below, recursionError := ReadNPMProjects(directory)
-			if recursionError != nil {
-				return recursionError
-			}
-			returned = append(returned, below...)
-		}
-		return nil
-	}
-	for _, entry := range entries {
-		if !entry.IsDir() {
-			continue
-		}
-		name := entry.Name()
-		log.Println(name)
-		if strings.HasPrefix(name, "@") { // ./node_modules/@scope/package
-			scope := name[1:]
-			scopePath := path.Join(node_modules, name)
-			scopeEntries, err := readAndStatDir(scopePath)
-			if err != nil {
-				if os.IsNotExist(err) {
-					continue
-				} else {
-					return nil, err
-				}
-			}
-			for _, scopeEntry := range scopeEntries {
-				if !scopeEntry.IsDir() {
-					continue
-				}
-				directory := path.Join(node_modules, name, scopeEntry.Name())
-				err := processProject(directory, &scope)
-				if err != nil {
-					return nil, err
-				}
-			}
-		} else { // ./node_modules/package/
-			directory := path.Join(node_modules, name)
-			err := processProject(directory, nil)
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-	return returned, nil
-}
-
-func alreadyHaveProject(projects []Project, projectID string) bool {
-	for _, other := range projects {
-		if other.Manifest.ProjectID == projectID {
-			return true
-		}
-	}
-	return false
-}
-
-func isDirSymlink(linkPath string) (bool, error) {
-	resolvedPath, err := os.Readlink(linkPath)
-	if err != nil {
-		return false, err
-	}
-	info, err := os.Stat(resolvedPath)
-	if err != nil {
-		return false, err
-	}
-	return info.IsDir(), nil
 }
 
 func isSymlink(entry os.FileInfo) bool {
---
+++ inventory/npm.go
@@ -0,0 +1,118 @@
+package inventory
+
+import "encoding/json"
+import "io/ioutil"
+import "os"
+import "path"
+import "strings"
+import "github.com/yookoala/realpath"
+
+type PackageJSONFile struct {
+	Name      string                   `json:"name"`
+	Version   string                   `json:"version"`
+	Envelopes []ProjectManifestEnvlope `json:"licensezero"`
+}
+
+type ProjectManifestEnvlope struct {
+	LicensorSignature string          `json:"agentSignature"`
+	AgentSignature    string          `json:"licensorSignature"`
+	Manifest          ProjectManifest `json:"license"`
+}
+
+func ReadNPMProjects(packagePath string) ([]Project, error) {
+	var returned []Project
+	node_modules := path.Join(packagePath, "node_modules")
+	entries, err := readAndStatDir(node_modules)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Project{}, nil
+		} else {
+			return nil, err
+		}
+	}
+	processProject := func(directory string, scope *string) error {
+		package_json := path.Join(directory, "package.json")
+		data, err := ioutil.ReadFile(package_json)
+		if err != nil {
+			return err
+		}
+		var parsed PackageJSONFile
+		json.Unmarshal(data, &parsed)
+		anyNewProjects := false
+		for _, envelope := range parsed.Envelopes {
+			if alreadyHaveProject(returned, envelope.Manifest.ProjectID) {
+				continue
+			}
+			anyNewProjects = true
+			project := Project{
+				Type:     "npm",
+				Path:     directory,
+				Name:     parsed.Name,
+				Version:  parsed.Version,
+				Manifest: envelope.Manifest,
+			}
+			realDirectory, err := realpath.Realpath(directory)
+			if err != nil {
+				project.Path = realDirectory
+			} else {
+				project.Path = directory
+			}
+			if scope != nil {
+				project.Scope = *scope
+			}
+			returned = append(returned, project)
+		}
+		if anyNewProjects {
+			below, recursionError := ReadNPMProjects(directory)
+			if recursionError != nil {
+				return recursionError
+			}
+			returned = append(returned, below...)
+		}
+		return nil
+	}
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			continue
+		}
+		name := entry.Name()
+		if strings.HasPrefix(name, "@") { // ./node_modules/@scope/package
+			scope := name[1:]
+			scopePath := path.Join(node_modules, name)
+			scopeEntries, err := readAndStatDir(scopePath)
+			if err != nil {
+				if os.IsNotExist(err) {
+					continue
+				} else {
+					return nil, err
+				}
+			}
+			for _, scopeEntry := range scopeEntries {
+				if !scopeEntry.IsDir() {
+					continue
+				}
+				directory := path.Join(node_modules, name, scopeEntry.Name())
+				err := processProject(directory, &scope)
+				if err != nil {
+					return nil, err
+				}
+			}
+		} else { // ./node_modules/package/
+			directory := path.Join(node_modules, name)
+			err := processProject(directory, nil)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return returned, nil
+}
+
+func alreadyHaveProject(projects []Project, projectID string) bool {
+	for _, other := range projects {
+		if other.Manifest.ProjectID == projectID {
+			return true
+		}
+	}
+	return false
+}
