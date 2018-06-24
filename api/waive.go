9691b0e6ed9c47add4b0948f740e755ede0f134b
Kyle E. Mitchell <kyle@kemitchell.com>
2018-06-23 22:23:53 -0700

    Implement waive subcommand

---
+++ api/waive.go
@@ -0,0 +1,40 @@
+package api
+
+import "bytes"
+import "encoding/json"
+import "errors"
+import "github.com/licensezero/cli/data"
+import "io/ioutil"
+import "net/http"
+
+type WaiveRequest struct {
+	Action       string `json:"action"`
+	LicensorID   string `json:"licensorID"`
+	Token        string `json:"token"`
+	ProjectID    string `json:"projectID"`
+	Beneficiary  string `json:"beneficiary"`
+	Jurisdiction string `json:"jurisdiction"`
+	Term         string `json:"term"`
+}
+
+func Waive(licensor *data.Licensor, projectID, beneficiary, jurisdiction, term string) ([]byte, error) {
+	bodyData := WaiveRequest{
+		Action:       "waive",
+		LicensorID:   licensor.LicensorID,
+		Token:        licensor.Token,
+		ProjectID:    projectID,
+		Beneficiary:  beneficiary,
+		Jurisdiction: jurisdiction,
+		Term:         term,
+	}
+	body, err := json.Marshal(bodyData)
+	if err != nil {
+		return nil, errors.New("Error serializing request body.")
+	}
+	response, err := http.Post("https://licensezero.com/api/v0", "application/json", bytes.NewBuffer(body))
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		return nil, errors.New("Server responded " + string(response.StatusCode))
+	}
+	return ioutil.ReadAll(response.Body)
+}
--- subcommands/waive.go
+++ subcommands/waive.go
@@ -1,37 +1,56 @@
 package subcommands
 
 import "flag"
-import "fmt"
+import "github.com/licensezero/cli/api"
+import "github.com/licensezero/cli/data"
 import "os"
-
-// TODO: Implement waive subcommand.
+import "strconv"
 
 var Waive = Subcommand{
 	Description: "Generate a signed waiver.",
 	Handler: func(args []string, paths Paths) {
-		flagSet := flag.NewFlagSet("waive", flag.ExitOnError)
-		jurisdiction := flagSet.Bool("jurisdiction", false, "Jurisdiction.")
-		days := flagSet.Int("days", 0, "Days.")
+		flagSet := flag.NewFlagSet("waive", flag.ContinueOnError)
+		jurisdiction := flagSet.String("jurisdiction", "", "Jurisdiction.")
+		days := flagSet.Uint("days", 0, "Days.")
 		forever := flagSet.Bool("forever", false, "Forever.")
-		beneficiary := flagSet.Bool("beneficiary", false, "Beneficiary legal name.")
-		flagSet.Parse(args)
+		beneficiary := flagSet.String("beneficiary", "", "Beneficiary legal name.")
+		err := flagSet.Parse(args)
+		if err != nil {
+			waiveUsage()
+		}
 		if *forever && *days > 0 {
 			waiveUsage()
-		} else if flagSet.NArg() != 1 {
+		} else if *days == 0 && !*forever {
+			waiveUsage()
+		} else if flagSet.NArg() != 1 || *beneficiary == "" || *jurisdiction == "" {
 			waiveUsage()
 		} else {
 			projectID := args[0]
-			fmt.Println(projectID)
-			fmt.Println(beneficiary)
-			fmt.Println(jurisdiction)
-			fmt.Println(days)
+			licensor, err := data.ReadLicensor(paths.Home)
+			if err != nil {
+				os.Stderr.WriteString("Create a licensor identity with `licensezero register` or `licensezero set-licensor-id`.")
+				os.Exit(1)
+			}
+			var term string
+			if *forever {
+				term = "forever"
+			} else {
+				term = strconv.Itoa(int(*days))
+			}
+			bytes, err := api.Waive(licensor, projectID, *beneficiary, *jurisdiction, term)
+			if err != nil {
+				os.Stderr.WriteString(err.Error())
+				os.Exit(1)
+			}
+			os.Stdout.Write(bytes)
 			os.Exit(0)
 		}
 	},
 }
 
 func waiveUsage() {
-	os.Stderr.WriteString(`Usage:
+	os.Stderr.WriteString(`Generate a signed waiver.
+Usage:
 	<project id> --beneficiary NAME --jurisdiction CODE (--days DAYS | --forever)
 
 Options:
