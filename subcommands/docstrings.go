421ba4558e1eb83fa0d42c8ad992d1e35addfbfb
Kyle E. Mitchell <kyle@kemitchell.com>
2018-06-24 00:21:21 -0700

    Correct licensor creation hint

--- subcommands/docstrings.go
+++ subcommands/docstrings.go
@@ -12,6 +12,6 @@ const relicenseLine = "Cost to relicense on permissive terms, in US cents."
 
 const projectIDLine = "Project ID (UUID)."
 
-const licensorHint = "Create a licensor identity with `licensezero register` or `licensezero set-licensor-id`."
+const licensorHint = "Create a licensor identity with `licensezero register`."
 
 const identityHint = "Create a licensor identity with `licensezero identify`."
