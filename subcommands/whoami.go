97dc4299da9626659f1a821f04a88aeb5c5bcfff
Kyle E. Mitchell <kyle@kemitchell.com>
2018-06-24 18:21:34 -0700

    whoami: Show licensor ID

--- subcommands/whoami.go
+++ subcommands/whoami.go
@@ -9,15 +9,18 @@ const whoAmIDescription = "Show your name, tax jurisdiction, and e-mail."
 var WhoAmI = Subcommand{
 	Description: whoAmIDescription,
 	Handler: func(args []string, paths Paths) {
-		data, err := data.ReadIdentity(paths.Home)
+		identity, err := data.ReadIdentity(paths.Home)
 		if err != nil {
 			os.Stderr.WriteString("Could not read identity file.\n")
 			os.Exit(1)
-		} else {
-			fmt.Println("Name: " + data.Name)
-			fmt.Println("Jurisdiction: " + data.Jurisdiction)
-			fmt.Println("E-Mail: " + data.EMail)
-			os.Exit(0)
 		}
+		licensor, err := data.ReadLicensor(paths.Home)
+		fmt.Println("Name: " + identity.Name)
+		fmt.Println("Jurisdiction: " + identity.Jurisdiction)
+		fmt.Println("E-Mail: " + identity.EMail)
+		if err == nil {
+			fmt.Println("Licensor ID: " + licensor.LicensorID)
+		}
+		os.Exit(0)
 	},
 }
