354b1c497b102e801bc6ca416769eae1d9bd3285
Kyle E. Mitchell <kyle@kemitchell.com>
2018-06-24 21:23:09 -0700

    Test identify subcommand

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import "bytes"
+import "io/ioutil"
+import "os"
 import "os/exec"
 import "strings"
 import "testing"
@@ -20,4 +22,46 @@ func TestSanity(t *testing.T) {
 	if !strings.Contains(output, "License Zero") {
 		t.Error("does not mention License Zero")
 	}
+}
+
+func TestIdentify(t *testing.T) {
+	InTestDir(t, func() {
+		command := exec.Command("./licensezero", "identify", "--name", "John Doe", "--jurisdiction", "US-CA", "--email", "text@example.com")
+		var stdout bytes.Buffer
+		command.Stdout = &stdout
+		err := command.Run()
+		if err != nil {
+			t.Error(err)
+		}
+		output := string(stdout.Bytes())
+		if !strings.Contains(output, "Saved") {
+			t.Error("Does not print \"Saved\"")
+		}
+	})
+}
+
+func TestIdentifySilent(t *testing.T) {
+	InTestDir(t, func() {
+		command := exec.Command("./licensezero", "identify", "--name", "John Doe", "--jurisdiction", "US-CA", "--email", "text@example.com", "--silent")
+		var stdout bytes.Buffer
+		command.Stdout = &stdout
+		err := command.Run()
+		if err != nil {
+			t.Error(err)
+		}
+		output := string(stdout.Bytes())
+		if output != "" {
+			t.Error("No output")
+		}
+	})
+}
+
+func InTestDir(t *testing.T, script func()) {
+	directory, err := ioutil.TempDir("/tmp", "licensezero-test")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(directory)
+	os.Setenv("LICENSEZERO_CONFIG", directory)
+	script()
 }
