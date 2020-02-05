8cfd2ca63135d2db3f75259131d36e4d8215c142
Kyle E. Mitchell <kyle@kemitchell.com>
2020-02-04 22:05:23 -0800

    Implement API offer record parsing

---
+++ offers.go
@@ -0,0 +1,54 @@
+package cli
+
+import (
+	"errors"
+	"github.com/mitchellh/mapstructure"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+// Offer represents an offer to sell licenses.
+type Offer struct {
+	URL        string  `json:"url"`
+	LicensorID string  `json:"licensorID"`
+	Pricing    Pricing `json:"pricing"`
+}
+
+// Pricing represents a price list.
+type Pricing struct {
+	Single    Price `json:"single"`
+	Relicense Price `json:"relicense"`
+}
+
+// Price represents a price asked or paid.
+type Price struct {
+	Amount   uint   `json:"amount"`
+	Currency string `json:"currency"`
+}
+
+func parseOffer(unstructured interface{}) (o *Offer, err error) {
+	if !validateOffer(unstructured) {
+		return nil, errors.New("invalid offer")
+	}
+	err = mapstructure.Decode(unstructured, &o)
+	return
+}
+
+var offerValidator *gojsonschema.Schema = nil
+
+func validateOffer(unstructured interface{}) bool {
+	if offerValidator == nil {
+		schema, err := schemaLoader().Compile(
+			gojsonschema.NewStringLoader(offerSchema),
+		)
+		if err != nil {
+			panic(err)
+		}
+		offerValidator = schema
+	}
+	dataLoader := gojsonschema.NewGoLoader(unstructured)
+	result, err := offerValidator.Validate(dataLoader)
+	if err != nil {
+		return false
+	}
+	return result.Valid()
+}
---
+++ offers_test.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestParseOffer(t *testing.T) {
+	url := "http://example.com/project"
+	licensorID := "6005a0cd-2481-468c-a53b-6e844930e413"
+	var amount uint = 500
+	currency := "USD"
+	data := fmt.Sprintf(`{"url": "%v", "licensorID": "%v", "pricing": { "single": { "amount": %v, "currency": "%v" } } }`, url, licensorID, amount, currency)
+
+	var unstructured interface{}
+	err := json.Unmarshal([]byte(data), &unstructured)
+	if err != nil {
+		t.Error(err)
+	}
+
+	offer, err := parseOffer(unstructured)
+	if err != nil {
+		t.Error(err)
+	}
+	if offer.URL != url {
+		t.Error("failed to parse URL")
+	}
+	if offer.LicensorID != licensorID {
+		t.Error("failed to parse licensor ID")
+	}
+	if offer.Pricing.Single.Amount != amount {
+		t.Error("failed to parse amount")
+	}
+	if offer.Pricing.Single.Currency != currency {
+		t.Error("failed to parse currency")
+	}
+}
