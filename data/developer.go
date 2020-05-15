dfe4d0d171fb32ec285f44ff76dc1f198f291abb
Kyle E. Mitchell <kyle@kemitchell.com>
2020-05-15 16:18:13 -0700

    Transparently upgrade old licensor.json to developer.json

--- data/developer.go
+++ data/developer.go
@@ -2,6 +2,7 @@ package data
 
 import "encoding/json"
 import "io/ioutil"
+import "os"
 import "path"
 
 // Developer describes a developer ID and access token.
@@ -19,11 +20,47 @@ func ReadDeveloper(home string) (*Developer, error) {
 	path := developerPath(home)
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		// Attempt to read legacy licensor.json file.
+		legacy, legacyErr := readLicensor(home)
+		if legacyErr != nil {
+			return nil, err
+		}
+		// If we have a legacy licensor.json file,
+		// write a a replacement developer.json file
+		// and delete the old licensor.json file.
+		upgraded := Developer{
+			Token:       legacy.Token,
+			DeveloperID: legacy.LicensorID,
+		}
+		writeErr := WriteDeveloper(home, &upgraded)
+		if writeErr == nil {
+			os.Remove(licensorPath(home))
+		}
+		return &upgraded, nil
 	}
 	var developer Developer
 	json.Unmarshal(data, &developer)
 	return &developer, nil
+}
+
+type legacyLicensor struct {
+	Token      string `json:"token"`
+	LicensorID string `json:"licensorID"`
+}
+
+func licensorPath(home string) string {
+	return path.Join(ConfigPath(home), "licensor.json")
+}
+
+func readLicensor(home string) (*legacyLicensor, error) {
+	path := licensorPath(home)
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var licensor legacyLicensor
+	json.Unmarshal(data, &licensor)
+	return &licensor, nil
 }
 
 // WriteDeveloper writes a developer ID and access token to disk.
